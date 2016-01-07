56cdea4e89ab7b46d58b694be492e4089f75dcba
Evan Tatum <evantatum@Evans-MacBook-Pro.local>
2016-01-06 16:42:32 -0800

    counter/loop fix

--- app.js
+++ app.js
@@ -1,30 +1,54 @@
-
 			var userName = prompt('What\'s your name friend ?');
       console.log(' The user\'s name is ' + userName);
 			alert(' Okay ' + userName + ', I hope you are here to learn about me. If not... forget it let\'s begin !');
 
-			var questionOne = function (answerOne) {
-				var	guessOne = prompt("Where do I want this class to take me \n A)Gaming industry, B) Just doing it for fun, C) No where ?").toUpperCase();
-					if (guessOne !== answerOne) alert("Try Again!");
-					while (guessOne !== answerOne); };
+			var firstTry= 0
 
-					questionOne("A");
-					alert("Yup! my end goal is to go into game development!");
 
-				var questionTwo = function (answerTwo) {
-				var guessTwo = prompt("How many siblings do I have ? A) none, B) 1, C) 3 .");
-				if (guessTwo !== answerTwo) alert("Try Again!");
-				while (guessTwo !== answerTwo);       };
+			var questionOne = prompt("Do I want to go into the game industry ? \
+				  Yes, or No ");
+					if (questionOne.toUpperCase() === 'Y' || questionOne.toUpperCase() === 'YES') {
+					alert("Right !My end goal is to go into game development!");
 
-				questionTwo("C");
-					alert("Lusky guess");
 
-			var questionThree = function (answerThree) {
-				var guessThree = prompt("Hm...I don\'t know what els to ask you. \n so pick a letter ! any letter. \n as long as it is B  \n A)1, B) 2, C)3 or ").toUpperCase();
-				if (guessThree !== answerThree) alert("Try Again!");
-				while (guessThree !== answerThree);     };
+					 firstTry+= 1
 
-				questionThree("B");
-					alert("Good Job!!");
 
+				} else {
+					alert( "nope")
+				}
+
+
+				var questionTwo = prompt("Do i have any siblings ? Yes or No");
+				if (questionTwo.toUpperCase() === "Y" || questionTwo.toUpperCase() === "yes") {
+				alert("Right, three brothers.");
+
+				firstTry += 1
+
+			} else {
+				alert("nope!")
+			}
+
+			var questionThree =  prompt(" Yes or No ? (yes is the answer)");
+				if (questionThree.toUpperCase() === "Y" || questionThree.toUpperCase() === "Yes") {
+				alert("Right, it would be hard to mess that one up");
+
+				firstTry += 1
+
+			} else {
+				alert("Come on man...")
+			}
+
+					var questionFour = prompt(" What is your favorite number between 1 and 10 ? ");
+						if (questionFour == 7) {
+						alert("Right!");
+						firstTry +=1
+					} else if(questionFour <=  7) {
+							alert('Nope, too low');
+					 	} else {
+							alert("Nope, too high");
+						}
 				alert("I know you are happy you know more about me \n see ya later !");
+
+
+				alert(firstTry + ' correct Answers ' );
