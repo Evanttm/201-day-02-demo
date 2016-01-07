cf583b3d6f05a0aa9337e2de341342b247839107
Ben Garnaat <garnaat.ben@gmail.com>
2016-01-07 13:45:59 -0800

    added some stuff, fixed a thing, did some other stuff. GREAT!

--- app.js
+++ app.js
@@ -1,54 +1,66 @@
-			var userName = prompt('What\'s your name friend ?');
-      console.log(' The user\'s name is ' + userName);
-			alert(' Okay ' + userName + ', I hope you are here to learn about me. If not... forget it let\'s begin !');
-
-			var firstTry= 0
-
-
-			var questionOne = prompt("Do I want to go into the game industry ? \
-				  Yes, or No ");
-					if (questionOne.toUpperCase() === 'Y' || questionOne.toUpperCase() === 'YES') {
-					alert("Right !My end goal is to go into game development!");
+var res1 = document.getElementById('resultOne');
+var res2 = document.getElementById('resultTwo');
+var res3 = document.getElementById('resultThree');
+var res4 = document.getElementById('resultFour');
 
 
-					 firstTry+= 1
+var userName = prompt('What\'s your name friend?');
+console.log('The user\'s name is ' + userName);
+alert('Okay ' + userName + ', I hope you are here to learn about me. If not... forget it let\'s begin!');
+
+var firstTry = 0;
+
+qOne();
+qTwo();
+qThree();
+qFour();
 
 
-				} else {
-					alert( "nope")
-				}
+function qOne() {
+	var questionOne = prompt('Do I want to go into the game industry? Yes or No');
+	if ((questionOne.toUpperCase() === "Y") || (questionOne.toUpperCase() === "YES")) {
+		firstTry++;
+		console.log(firstTry);
+		res1.textContent = 'Right!  My end goal is to go into game development!';
+	} else {
+		res1.textContent = 'nope';
+	}
+}
 
+function qTwo() {
+	var questionTwo = prompt('Do i have any siblings? Yes or No');
+	if ((questionTwo.toUpperCase() === 'Y') || (questionTwo.toUpperCase() === 'YES')) {``
+		firstTry++;
+		console.log(firstTry);
+		res2.textContent = 'Right, three brothers.';
+	} else {
+		res2.textContent = 'nope!';
+	}
+}
 
-				var questionTwo = prompt("Do i have any siblings ? Yes or No");
-				if (questionTwo.toUpperCase() === "Y" || questionTwo.toUpperCase() === "yes") {
-				alert("Right, three brothers.");
+function qThree() {
+	var questionThree =  prompt('Yes or No ? (yes is the answer)');
+	if ((questionThree.toUpperCase() === 'Y') || (questionThree.toUpperCase() === 'YES')) {
+		res3.textContent = 'Right, it would be hard to mess that one up';
+		firstTry++;
+		console.log(firstTry);
+	} else {
+		res3.textContent = 'Come on man...';
+	}
+}
 
-				firstTry += 1
+function qFour() {
+	var questionFour = prompt('What is your favorite number between 1 and 10?');
+	if (questionFour == 7) {
+		res4.textContent = 'Right!';
+		firstTry++;
+		console.log(firstTry);
+	} else if(questionFour <  7) {
+		res4.textContent = 'Nope, too low';
+	} else {
+		res4.textContent = 'Nope, too high';
+	}
+}
+	alert('I know you are happy you know more about me \n see ya later!');
 
-			} else {
-				alert("nope!")
-			}
-
-			var questionThree =  prompt(" Yes or No ? (yes is the answer)");
-				if (questionThree.toUpperCase() === "Y" || questionThree.toUpperCase() === "Yes") {
-				alert("Right, it would be hard to mess that one up");
-
-				firstTry += 1
-
-			} else {
-				alert("Come on man...")
-			}
-
-					var questionFour = prompt(" What is your favorite number between 1 and 10 ? ");
-						if (questionFour == 7) {
-						alert("Right!");
-						firstTry +=1
-					} else if(questionFour <=  7) {
-							alert('Nope, too low');
-					 	} else {
-							alert("Nope, too high");
-						}
-				alert("I know you are happy you know more about me \n see ya later !");
-
-
-				alert(firstTry + ' correct Answers ' );
+	alert(firstTry + ' correct Answers');
